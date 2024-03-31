7d908e612572867e27801f7ba689726c1d65dd01
imran <zunaeede@gmail.com>
2024-03-31 16:21:45 +0600

    main - finally add the all button and make it work.

--- README.md
+++ README.md
@@ -3,6 +3,20 @@ i will practice counter App using react and redux.
 
 - [Tutorial for this App.](https://youtu.be/paY1O3oD3gg?si=OEXbhwq5pad3Kk6X)
 
+### Part by part Process
+- state - count: 0
+- actions - increment- decrement - reset
+- reducers - increment - count - count+1
+- store
+- providing store in react
+- use store
+- 
+
+### Some function work
+- `useSelector()` this rect-redux function can take the state in the page we need.
+- `useDispatch()` we use this function for dispatch in react form redux. 
+
+
 ### Install or Setup Process.
 - First we install it by using react vite using it.
 - than we install `npm install redux`.
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { decrementCounter, incrementCounter, resetCounter } from '../services/actions/counterAction';
 
 
 const Counter = () => {
+// we use this function for taking the state value.
+  const count = useSelector(state => state.count)
+
+  // we use this function for dispatch.
+  const dispatch = useDispatch();
+
+
+  const handleIncrement = () => {
+    dispatch(incrementCounter());
+  };
+  const handleDecrement = () => {
+    dispatch(decrementCounter());
+  };
+  const handleReset = () => {
+    dispatch(resetCounter());
+  }
 
 
   return (
     <>
       <h2>Counter App</h2>
 
-      <h3>Count: </h3>
-      <button >Increment</button>
+      <h3>Count: {count}</h3>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 }
