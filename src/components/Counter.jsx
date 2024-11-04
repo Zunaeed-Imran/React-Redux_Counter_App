36be7d27ec33b8fc3b7d75c14d2bd082a66a7b65
imran <zunaeede@gmail.com>
2024-11-04 09:25:39 +0600

    main - remove unnecessary import.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { useDispatch, useSelector } from 'react-redux';
 import { decrementCounter, incrementCounter, resetCounter } from '../services/actions/counterAction';
 
